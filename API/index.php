95106466b2c242c03fb4b9b61d7c93d19426178f
Salvatore Iaconesi <xdxd.vs.xdxd@gmail.com>
2020-07-16 12:40:19 +0200

    Update index.php

--- API/index.php
+++ API/index.php
@@ -171,7 +171,7 @@ function doCreateCouples($request,$conn){
 	$users = array();
 
 	for($i = 0; $i<count($u); $i++){
-		$users[] = $u[$i]["id"];
+		$users[] = $u[$i]->userid;
 	}
 
 	$result->users = $u;
